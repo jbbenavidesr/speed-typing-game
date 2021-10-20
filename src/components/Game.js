73746df06c91463654ebabdacc224440571ca610
Juan Bernardo Benavides Rubio <juan.benavides@fintegra.co>
2021-10-20 14:56:31 -0500

    Mvp, needs some disabling

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,15 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Game(props) {
+    const TIME_PER_GAME = 5;
+
     const [text, setText] = useState("");
+    const [timeRemaining, setTimeRemaining] = useState(TIME_PER_GAME);
+    const [wordCount, setWordCount] = useState(0);
+    const [isGameRunning, setIsGameRunning] = useState(false);
 
     function handleChange(e) {
         setText(e.target.value);
     }
 
+    useEffect(
+        function () {
+            if (isGameRunning && timeRemaining > 0) {
+                setTimeout(function () {
+                    setTimeRemaining(function (time) {
+                        return time - 1;
+                    });
+                }, 1000);
+            } else if (timeRemaining === 0) {
+                setWordCount(countWords(text));
+            }
+        },
+        [timeRemaining, isGameRunning]
+    );
+
     function countWords(text) {
         let count = text.split(/\s+/).filter(function (word) {
-            return !!word;
+            return word.length;
         }).length;
         return count;
     }
@@ -17,9 +37,9 @@ function Game(props) {
     return (
         <>
             <textarea value={text} onChange={handleChange} />
-            <p>Time remaining: ???</p>
-            <button onClick={() => countWords(text)}>Start</button>
-            <p>Word Count: </p>
+            <p>Time remaining: {timeRemaining}</p>
+            <button onClick={() => setIsGameRunning(true)}>Start</button>
+            <p>Word Count: {wordCount} </p>
         </>
     );
 }
